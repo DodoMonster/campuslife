3c0eac92c5e94d2ddebc01814a1ea9c3b74d3a1b
DodoMonster <716282978@qq.com>
2016-02-27 13:03:05 +0800

    终极版1.0

--- js/disadvantages.js
+++ js/disadvantages.js
@@ -40,42 +40,43 @@ $(function(){
         else {
             $("ul.nav").fadeOut();
         }
-
-        var dis_1=$("#item_1").offset().top-80,
-            dis_2=$("#item_2").offset().top-80,
-            dis_3=$("#item_3").offset().top-80,
-            dis_4=$("#item_4").offset().top-80,
-            dis_5=$("#item_5").offset().top-80,
-            dis_6=$("#item_6").offset().top-80;
-            console.log(scTop,dis_1,dis_2,dis_3,dis_4);
-            if(scTop<dis_1){
-                $navLi.removeClass("active");
-                $navLi.eq(0).addClass("active");
-            }
-            else if(scTop<=dis_2){
-                $navLi.removeClass("active");
-                $navLi.eq(6).addClass("active");
-            }
-            else if(scTop<=dis_3){
-                $navLi.removeClass("active");
-                $navLi.eq(5).addClass("active");
-            }
-            else if(scTop<=dis_4){
-                $navLi.removeClass("active");
-                $navLi.eq(4).addClass("active");
-            }
-            else if(scTop<=dis_5){
-                $navLi.removeClass("active");
-                $navLi.eq(3).addClass("active");
-            }
-            else if(scTop<=dis_6){
-                $navLi.removeClass("active");
-                $navLi.eq(2).addClass("active");
-            }
-            else{
-                $navLi.removeClass("active");
-                $navLi.eq(1).addClass("active");
-            }
     })
-
 })
+$(".menu").click(function(){
+    if ($(".navigation").is(":hidden")){
+        $(".navigation").slideDown();
+        $(".menu").addClass("menuGo");
+    }
+    else{
+        $(".navigation").slideUp();
+        $(".menu").removeClass("menuGo");
+    }
+});
+
+function imgReload()
+{
+    var imgHeight = 0;
+    var wtmp = $("body").width();
+    $(".banner ul li").css({width:wtmp + "px"});
+    // $(".banner ul").css({height:900+'px'});
+}
+
+$(window).resize(
+    function(){
+        imgReload();
+    }
+);
+
+$(document).ready(function(e) {
+    imgReload();
+    var unslider = $('.banner').unslider({
+        dots: true,
+        fluid: true
+    }),
+    data = unslider.data('unslider');
+
+    $('.unslider-arrow').click(function() {
+        var fn = this.className.split(' ')[1];
+        data[fn]();
+    });
+});
